9807d5917c4db9150e10981e3108273102be5af3
Kim CG <cjfrbs1256@gmail.com>
2023-10-17 21:08:42 +0900

    second commit

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdminPage = () => {
+  return (
+    <div>관리자만 유저만 볼수있는 페이지입니다.</div>
+  )
+}
+
+export default AdminPage
---
+++ src/app/user/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserPage = () => {
+  return (
+    <div>로그인된 유저만 볼수있는 페이지입니다.</div>
+  )
+}
+
+export default UserPage
